aeaa650a235847fc29375019b308eeb27bb0d66f
unknown <juan.cepeda-s@labinfo.is.escuelaing.edu.co>
2023-09-13 10:58:19 -0500

    .

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/submuestreoBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/submuestreoBlueprintPersistence.java
@@ -1,16 +1,34 @@
 package edu.eci.arsw.blueprints.persistence.impl;
 
-import java.util.Set;
-
-import org.springframework.stereotype.Service;
-
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
 
-@Service
-public class submuestroeBlueprintPersistence implements BlueprintsPersistence {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hcadavid
+ */
+//@Service
+public class submuestreoBlueprintPersistence implements BlueprintsPersistence {
+
+    private final Map<Tuple<String, String>, Blueprint> blueprints = new HashMap<>();
+
+    public submuestreoBlueprintPersistence() {
+        // load stub data
+        Point[] pts = new Point[] { new Point(140, 140), new Point(115, 115), new Point(100, 100), new Point(90, 90), new Point(80, 80), new Point(70, 70) };
+        Blueprint bp = new Blueprint("prueba", "funca? ", pts);
+        blueprints.put(new Tuple<>(bp.getAuthor(), bp.getName()), bp);
+
+    }
 
     @Override
     public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
@@ -20,8 +38,10 @@ public class submuestroeBlueprintPersistence implements BlueprintsPersistence {
 
     @Override
     public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBlueprint'");
+       Blueprint bp = blueprints.get(new Tuple<>(author, bprintname));
+        for(int i = 0; i <= bp.getPoints().size(); i ++){
+            
+        }
     }
 
     @Override
